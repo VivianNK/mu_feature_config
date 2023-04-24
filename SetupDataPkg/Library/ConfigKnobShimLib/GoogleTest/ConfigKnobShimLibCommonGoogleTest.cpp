37c67fd97b9bc25dcefd11e9d8bff5013fabdb49
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2023-04-24 16:28:41 -0700

    added invalid param testing

--- SetupDataPkg/Library/ConfigKnobShimLib/GoogleTest/ConfigKnobShimLibCommonGoogleTest.cpp
+++ SetupDataPkg/Library/ConfigKnobShimLib/GoogleTest/ConfigKnobShimLibCommonGoogleTest.cpp
@@ -16,25 +16,51 @@ extern "C" {
   #include <Library/DebugLib.h>
 }
 
+#define CONFIG_KNOB_GUID  {0x52d39693, 0x4f64, 0x4ee6, {0x81, 0xde, 0x45, 0x89, 0x37, 0x72, 0x78, 0x55}}
+
 using namespace testing;
 
-class SetSecureBootModeTest : public Test {
+class GetConfigKnobOverrideTest : public Test {
   protected:
     MockUefiRuntimeServicesTableLib RtServicesMock;
+    EFI_GUID    ConfigKnobGuid;
     EFI_STATUS  Status;
+    CHAR16      *ConfigKnobName;
+    UINTN       ProfileDefaultSize;
+    UINT64      ConfigKnobData;
 
     void SetUp() override {
-      // Any random magic number can be used for these tests
-      SecureBootMode = 0xAB;
+      ConfigKnobGuid      = CONFIG_KNOB_GUID;
+      ConfigKnobName      = L"MyDeadBeefDelivery";
+      ProfileDefaultSize  = sizeof (ConfigKnobData);
     }
 };
 
-TEST_F(SetSecureBootModeTest, SetVarError) {
-  EXPECT_CALL(RtServicesMock, gRT_SetVariable)
-    .WillOnce(Return(EFI_INVALID_PARAMETER));
+/**
+ * todo - all these tests are testing, what happens when the getvariable call returns EFI_INVALID_PARAMETER
+ * @ Oliver is there anything else in GetConfigKnobbOverride you wanted to test? that the data is not changed if the params are invalid?
+*/
+TEST_F(GetConfigKnobOverrideTest, InvalidParamError) {
+  EXPECT_CALL(RtServicesMock, gRT_GetVariable)
+    .Times(4)
+    .WillRepeatedly(Return(EFI_INVALID_PARAMETER));
 
-  Status = SetSecureBootMode(SecureBootMode);
+  // Variable name is NULL
+  Status = GetConfigKnobOverride (&ConfigKnobGuid, NULL, (VOID *)&ConfigKnobData, ProfileDefaultSize);
+  EXPECT_EQ (Status, EFI_INVALID_PARAMETER);
+
+  // Vendor GUID is NULL
+  Status = GetConfigKnobOverride (NULL, ConfigKnobName, (VOID *)&ConfigKnobData, ProfileDefaultSize);
+  EXPECT_EQ (Status, EFI_INVALID_PARAMETER);
+
+  // DataSize is NULL
+  Status = GetConfigKnobOverride (&ConfigKnobGuid, ConfigKnobName, (VOID *) &ConfigKnobData, 0);
   EXPECT_EQ(Status, EFI_INVALID_PARAMETER);
+
+  // DataSize is not too small and the Data is NULL
+  Status = GetConfigKnobOverride (&ConfigKnobGuid, ConfigKnobName, NULL, ProfileDefaultSize);
+  EXPECT_EQ (Status, EFI_INVALID_PARAMETER);
+
 }
 
 int main(int argc, char* argv[]) {
