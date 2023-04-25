e5a8af785a985aba1cf494135669d70d4f36bd1a
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2023-04-25 14:40:12 -0700

    fixed invalid param test, started override from var storage test

--- SetupDataPkg/Library/ConfigKnobShimLib/GoogleTest/ConfigKnobShimLibCommonGoogleTest.cpp
+++ SetupDataPkg/Library/ConfigKnobShimLib/GoogleTest/ConfigKnobShimLibCommonGoogleTest.cpp
@@ -22,7 +22,6 @@ using namespace testing;
 
 class GetConfigKnobOverrideTest : public Test {
   protected:
-    MockUefiRuntimeServicesTableLib RtServicesMock;
     EFI_GUID    ConfigKnobGuid;
     EFI_STATUS  Status;
     CHAR16      *ConfigKnobName;
@@ -39,28 +38,62 @@ class GetConfigKnobOverrideTest : public Test {
 /**
  * todo - all these tests are testing, what happens when the getvariable call returns EFI_INVALID_PARAMETER
  * @ Oliver is there anything else in GetConfigKnobbOverride you wanted to test? that the data is not changed if the params are invalid?
+ * if want them all, should break out into separate TEST_Fs
 */
 TEST_F(GetConfigKnobOverrideTest, InvalidParamError) {
-  EXPECT_CALL(RtServicesMock, gRT_GetVariable)
-    .Times(4)
-    .WillRepeatedly(Return(EFI_INVALID_PARAMETER));
 
   // Variable name is NULL
   Status = GetConfigKnobOverride (&ConfigKnobGuid, NULL, (VOID *)&ConfigKnobData, ProfileDefaultSize);
-  EXPECT_EQ (Status, EFI_INVALID_PARAMETER);
+  ASSERT_EQ (Status, EFI_INVALID_PARAMETER);
 
   // Vendor GUID is NULL
   Status = GetConfigKnobOverride (NULL, ConfigKnobName, (VOID *)&ConfigKnobData, ProfileDefaultSize);
-  EXPECT_EQ (Status, EFI_INVALID_PARAMETER);
+  ASSERT_EQ (Status, EFI_INVALID_PARAMETER);
 
   // DataSize is NULL
   Status = GetConfigKnobOverride (&ConfigKnobGuid, ConfigKnobName, (VOID *) &ConfigKnobData, 0);
-  EXPECT_EQ(Status, EFI_INVALID_PARAMETER);
+  ASSERT_EQ (Status, EFI_INVALID_PARAMETER);
 
   // DataSize is not too small and the Data is NULL
   Status = GetConfigKnobOverride (&ConfigKnobGuid, ConfigKnobName, NULL, ProfileDefaultSize);
-  EXPECT_EQ (Status, EFI_INVALID_PARAMETER);
+  ASSERT_EQ (Status, EFI_INVALID_PARAMETER);
 
+}
+
+class GetConfigKnobOverrideFromVariableStorageTest : public Test {
+  protected:
+    MockUefiRuntimeServicesTableLib RtServicesMock;
+    
+    EFI_STATUS  Status;
+    EFI_GUID    ConfigKnobGuid;
+    CHAR16      *ConfigKnobName;
+    UINTN       ProfileDefaultSize;
+    UINT64      ProfileDefaultValue;
+    UINTN       ProfileDefaultSize;
+    UINT64      VariableData;
+    PPI_STATUS  PpiStatus;
+    UINT64      ConfigKnobData;
+
+    void SetUp() override {
+      ConfigKnobGuid      = CONFIG_KNOB_GUID;
+      ConfigKnobName      = L"MyDeadBeefDelivery";
+      ProfileDefaultSize  = sizeof (ConfigKnobData);
+      ProfileDefaultValue = 0xDEADBEEFDEADBEEF;
+      ProfileDefaultSize  = sizeof (ProfileDefaultValue);
+      VariableData        = 0xBEEF7777BEEF7777;
+      PpiStatus           = { .Ppi = &MockVariablePpi, .Status = EFI_SUCCESS };
+
+    }
+};
+
+TEST_F(GetConfigKnobOverrideTest, VariableStorageSuccess) {
+  EXPECT_CALL(RtServicesMock, gRT_GetVariable)
+    .WillRepeatedly(Return(EFI_INVALID_PARAMETER));
+
+  // TODO need mock of PPI services from mdemodule
+  // temorarily can cvreate local mock?
+  Status = GetConfigKnobOverride (&ConfigKnobGuid, NULL, (VOID *)&ConfigKnobData, ProfileDefaultSize);
+  EXPECT_EQ (Status, EFI_INVALID_PARAMETER);
 }
 
 int main(int argc, char* argv[]) {
