e81c77477c2f56e89eb407e103d3857f93e57050
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2023-05-01 19:11:19 -0700

    introduce extern from DxeLibGoogleTest.cpp

--- SetupDataPkg/Library/ConfigKnobShimLib/ConfigKnobShimDxeLib/GoogleTest/ConfigKnobShimDxeLibGoogleTest.cpp
+++ SetupDataPkg/Library/ConfigKnobShimLib/ConfigKnobShimDxeLib/GoogleTest/ConfigKnobShimDxeLibGoogleTest.cpp
@@ -1,27 +1,25 @@
-// /** @file ConfigKnobShimLibCommonGoogleTest.cpp
-//   Google tests of the ConfigKnobShimLibCommon instance.
+/** @file ConfigKnobShimDxeLibCommonGoogleTest.cpp
+  Google tests of the ConfigKnobShimLibCommon instance.
 
-//   Copyright (c) Microsoft Corporation.
-//   SPDX-License-Identifier: BSD-2-Clause-Patent
+  Copyright (c) Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 
-// **/
+**/
 
-// #include <gtest/gtest.h>
-// #include <GoogleTest/Library/MockUefiRuntimeServicesTableLib.h>
+#include <gtest/gtest.h>
+#include <GoogleTest/Library/MockUefiRuntimeServicesTableLib.h>
 
-// extern "C" {
-//   #include <Uefi.h>
-//   #include <Library/BaseLib.h>
-//   #include <Library/DebugLib.h>
-//   // include the c file to be able to unit test static function
-//   #include "../../ConfigKnobShimLibCommon.h"
-// }
+extern "C" {
+  #include <Uefi.h>
+  #include <Library/BaseLib.h>
+  #include <Library/DebugLib.h>
+  // include the c file to be able to unit test static function
+  #include "../../ConfigKnobShimLibCommon.h"
+}
 
-// #define CONFIG_KNOB_GUID  {0x52d39693, 0x4f64, 0x4ee6, {0x81, 0xde, 0x45, 0x89, 0x37, 0x72, 0x78, 0x55}}
+using namespace testing;
 
-// using namespace testing;
-
-// extern MockUefiRuntimeServicesTableLib RtServicesMock;
+extern MockUefiRuntimeServicesTableLib RtServicesMock;
 
 // // TODO maaybe need RtSeervicesMock in header file ?then it's initialized in common?
 // // helper function with expect call to return EFI_NOT_FOUND
